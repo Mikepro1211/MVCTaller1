53da9a5cfa70e93c94b1bfb5fd991d0c68836251
Mikepro1211 <miguelalexander890@gmail.com>
2023-02-26 19:56:37 -0600

    prueba

--- app/src/main/java/com/example/tallerpractico1/MainActivity.kt
+++ app/src/main/java/com/example/tallerpractico1/MainActivity.kt
@@ -9,26 +9,10 @@ import android.widget.TextView
 import com.example.tallerpractico1.controller.Controller
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var nombreEditText: EditText
-    private lateinit var salarioEditText: EditText
-    private lateinit var calcularButton: Button
-    private lateinit var resultadoTextView: TextView
-    private lateinit var controlador: Controller
-    //@SuppressLint("MissingInflatedId")
-    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        nombreEditText = findViewById(R.id.etnombre)
-        salarioEditText = findViewById(R.id.etsalario)
-        calcularButton = findViewById(R.id.btncalcular)
-        resultadoTextView = findViewById(R.id.tvresultado)
 
-        calcularButton.setOnClickListener{
-            val nombre = nombreEditText.text.toString()
-            val salarioBase = salarioEditText.text.toString().toDouble()
-            val salarioNeto = controlador.calcularSalario(nombre,salarioBase)
-            resultadoTextView.text = "El salario neto de $nombre es $salarioNeto"
         }
+
     }
-}
---
+++ app/src/main/java/com/example/tallerpractico1/Salario.kt
@@ -0,0 +1,25 @@
+package com.example.tallerpractico1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//import android.annotation.SuppressLint
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.example.tallerpractico1.controller.Controller
+
+class Salario : AppCompatActivity() {
+    private lateinit var nombreEditText: EditText
+    private lateinit var salarioEditText: EditText
+    private lateinit var calcularButton: Button
+    private lateinit var resultadoTextView: TextView
+    private lateinit var controlador: Controller
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_salario)
+        nombreEditText = findViewById(R.id.etnombre)
+        salarioEditText = findViewById(R.id.etsalario)
+        calcularButton = findViewById(R.id.btncalcular)
+        resultadoTextView = findViewById(R.id.tvresultado)
+    }
+}
